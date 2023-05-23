b696fb4321495c1d855a51a71f4bcf987e7e96f9
NerijusBun <nerijuz@gmail.com>
2023-05-23 14:30:27 +0300

    for

---
+++ 003/index.php
@@ -0,0 +1,15 @@
+<?php
+
+// For ciklas
+
+for ($a = 1; $a <= 5; $a++) {
+    echo '<b>Didziojo ciklo Numeris: '.$a.' </b><br>';
+
+    for ($x = 1; $x <= 5; $x++) {
+    echo 'Mazojo ciklo Numeris: '.$x.' <br>';
+
+    }
+
+}
+ 
+
