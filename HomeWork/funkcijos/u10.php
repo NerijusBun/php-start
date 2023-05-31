3378779ffe0c89adcff2602ceb2ef3c2d4d38d07
NerijusBun <nerijuz@gmail.com>
2023-05-31 11:12:43 +0300

    uzdaviniai

---
+++ HomeWork/funkcijos/u10.php
@@ -0,0 +1,10 @@
+<?php
+
+echo '<pre>';
+
+//Sugeneruokite masyvą iš 10 elementų, kurie yra masyvai iš 10 elementų, kurie yra atsitiktiniai skaičiai nuo 1 iki 100. 
+//Jeigu tokio didelio masyvo (ne atskirai mažesnių) pirminių skaičių vidurkis mažesnis už 70, 
+//suraskite masyve mažiausią skaičių (nebūtinai pirminį) ir prie jo pridėkite 3. 
+//Vėl paskaičiuokite masyvo pirminių skaičių vidurkį ir jeigu mažesnis nei 70 viską kartokite. 
+
+
