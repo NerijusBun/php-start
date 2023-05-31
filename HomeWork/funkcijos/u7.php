23fd60d2532b76ea28333189fc78c8c82abd8330
NerijusBun <nerijuz@gmail.com>
2023-05-31 12:59:41 +0300

    uzdaviniai

--- HomeWork/funkcijos/u7.php
+++ HomeWork/funkcijos/u7.php
@@ -4,21 +4,39 @@ echo '<pre>';
 
 //Sugeneruokite atsitiktinio (nuo 10 iki 20) ilgio masyvą, kurio visi, išskyrus paskutinį, elementai yra atsitiktiniai skaičiai nuo 0 iki 10, o paskutinis masyvas, kuris generuojamas pagal tokią pat salygą kaip ir pirmasis masyvas. Viską pakartokite atsitiktinį nuo 10 iki 30  kiekį kartų. Paskutinio masyvo paskutinis elementas yra lygus 0;
 
-function makeArray($maxCount) {
-    static $counter = 0;
-    $subArray = [];
-    for($i = 1; $i <= 9; $i++){
-        $subArray[]= rand(0, 10);
+//function makeArray($maxCount) {
+//    static $counter = 0;
+//    $subArray = [];
+//    for($i = 1; $i <= 9; $i++){
+//        $subArray[]= rand(0, 10);
+//    }
+//    $counter++;
+//    if ($counter < $maxCount){
+//        $subArray[] = makeArray($maxCount);
+//    } else {
+//        $subArray[] = 0;
+//    }
+//return $subArray;
+//}
+//
+//$array[] = makeArray(rand(10,30));
+//
+//print_r($array);
+
+$size = rand(10, 30);
+$all = 0;
+
+for ($i = 0; $i < $size; $i++) {
+    
+    $howBig = rand(10, 20);
+    $small = [];    
+    for ($k = 0; $k < $howBig - 1; $k++) {
+        $small[] = rand(0, 10);
     }
-    $counter++;
-    if ($counter < $maxCount){
-        $subArray[] = makeArray($maxCount);
-    } else {
-        $subArray[] = 0;
-    }
-return $subArray;
+    $small[] = $all;
+    $all = $small;    
 }
 
-$array[] = makeArray(rand(10,30));
+echo '<pre>';
+print_r($all);
 
-print_r($array);
